380797a8bd591ac00e04745e7d3eae6951e2e8af
ISTEK <tamdacoder@gmail.com>
2025-08-01 16:59:14 -0500

    Update gen.py

--- wallet/gen.py
+++ wallet/gen.py
@@ -6,6 +6,7 @@ import ecdsa
 pk = ""
 prk = ""
 wallet = ""
+wallet2 = ""
 
 def begin():
   global pk, prk, wallet
@@ -14,7 +15,7 @@ def begin():
 
   sk = ecdsa.SigningKey.from_string(prkb, curve=ecdsa.SECP256k1)
   vk = sk.verifying_key
-  pkb = b'\x04' + vk.to_string()
+  pkb = vk.to_string()
   pk = pkb.hex()
 
   salty = os.urandom(10)
@@ -22,26 +23,19 @@ def begin():
   b58 = base58.b58encode(she).decode()
   wallet = "X" + b58 + "TST"
 
-  print("PRIVATE KEY: " + prk)
-  print("PUBLIC KEY: " + pk)
-  print("YOUR WALLET (TESTNET): " + wallet)
-
 def priv():
-  global pk, prk, wallet
+  global pk, prk, wallet, wallet2
   prkb = os.urandom(32)
   prk = prkb.hex()
 
   sk = ecdsa.SigningKey.from_string(prkb, curve=ecdsa.SECP256k1)
   vk = sk.verifying_key
-  pkb = b'\x04' + vk.to_string()
+  pkb = vk.to_string()
   pk = pkb.hex()
 
   salty = os.urandom(10)
   she = hashlib.sha256(pkb + salty).digest()
   b58 = base58.b58encode(she).decode()
   wallet = "X" + b58 + "TSP"
-
-  print("PRIVATE KEY: " + prk)
-  print("PUBLIC KEY: " + pk)
-  print("YOUR WALLET (PRIVATE): " + wallet)
+  wallet2 = "X" + base58.b56encode(hashlib.sha256(pkb + os.urandom(10)).digest()) + "TSP"
   

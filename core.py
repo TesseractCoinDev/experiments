7ed4f741a0fc1cda83eb4dd975d00616e0bd1af1
ISTEK <tamdacoder@gmail.com>
2025-08-01 16:37:40 -0500

    Update core.py

--- core.py
+++ core.py
@@ -1,14 +1,22 @@
 import time
 from wallet import gen
+from wallet import priv
 
 def menu():
   print("MENU")
   print("---")
   print("[GEN] - Wallet Generation Test")
+  print("[XTSP] - Private Transaction Test")
   time.sleep(1)
   choose = input(">> ")
   if choose.upper() == "[GEN]":
     gen.begin()
+    time.sleep(1)
+    menu()
+  elif choose.upper() == "[XTSP]":
+    priv.begin()
+    time.sleep(1)
+    menu()
   else:
     print("ERR - FUNCTION DOSENT EXIST")
     time.sleep(1)

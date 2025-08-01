05833dc34785b6f1f98e5598c05ff361b3cc8d51
ISTEK <tamdacoder@gmail.com>
2025-08-01 11:40:44 -0500

    now I know how it works (hopeofullyl)

--- wallet/gen.py
+++ wallet/gen.py
@@ -11,5 +11,18 @@ def begin():
   global pk, prk, wallet
   prkb = os.urandom(32)
   prk = prkb.hex()
-  # shi i forgot how this works, brb
+
+  sk = ecdsa.SigningKey.from_string(prkb, curve=ecdsa.SECP256k1)
+  vk = sk.verifying_key
+  pkb = b'\x04' + vk.to_string()
+  pk = pkb.hex()
+
+  salty = os.urandom(10)
+  she = hashlib.sha256(pkb + salty).digest()
+  b58 = base58.b58encode(she).decode()
+  wallet = "X" + b58 + "TST"
+
+  print("PRIVATE KEY: " + prk)
+  print("PUBLIC KEY: " + pk)
+  print("YOUR WALLET (TESTNET): " + wallet)
   

04b7e0a4ed1529312464078730f6969e9191b2c5
ISTEK <tamdacoder@gmail.com>
2025-08-06 22:15:51 -0500

    Replaced SHA-256 with Keccak-256, this is for future ProgPoW adoption :D

--- wallet/gen.py
+++ wallet/gen.py
@@ -1,4 +1,4 @@
-import hashlib
+import sha3
 import os
 import base58
 import ecdsa
@@ -6,8 +6,6 @@ import ecdsa
 pk = ""
 prk = ""
 wallet = ""
-wallet2 = ""
-sk = sk
 
 def begin():
   global pk, prk, wallet
@@ -20,23 +18,7 @@ def begin():
   pk = pkb.hex()
 
   salty = os.urandom(10)
-  she = hashlib.sha256(pkb + salty).digest()
+  she = sha3.keccak_256(pkb + salty).digest()
   b58 = base58.b58encode(she).decode()
   wallet = "X" + b58 + "TST"
-
-def priv():
-  global pk, prk, wallet, wallet2, sk
-  prkb = os.urandom(32)
-  prk = prkb.hex()
-
-  sk = ecdsa.SigningKey.from_string(prkb, curve=ecdsa.SECP256k1)
-  vk = sk.verifying_key
-  pkb = vk.to_string()
-  pk = pkb.hex()
-
-  salty = os.urandom(10)
-  she = hashlib.sha256(pkb + salty).digest()
-  b58 = base58.b58encode(she).decode()
-  wallet = "X" + b58 + "TSP"
-  wallet2 = "X" + base58.b56encode(hashlib.sha256(pkb + os.urandom(10)).digest()) + "TSP"
   

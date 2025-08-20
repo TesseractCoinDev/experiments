cee0d1c799288db68b238439e605066b6d9b1268
ISTEK <tamdacoder@gmail.com>
2025-08-19 21:43:21 -0500

    Update gen.py

--- wallet/gen.py
+++ wallet/gen.py
@@ -1,3 +1,4 @@
+from mnemonic import Mnemonic
 import sha3
 import os
 import base58
@@ -6,11 +7,14 @@ import ecdsa
 pk = ""
 prk = ""
 wallet = ""
+seed = ""
+init = Mnemonic("english")
 
 def begin():
   global pk, prk, wallet
   prkb = os.urandom(32)
   prk = prkb.hex()
+  seed = init.to_mnemonic(prkb)
 
   sk = ecdsa.SigningKey.from_string(prkb, curve=ecdsa.SECP256k1)
   vk = sk.verifying_key
@@ -21,4 +25,7 @@ def begin():
   she = sha3.keccak_256(pkb + salty).digest()
   b58 = base58.b58encode(she).decode()
   wallet = "X" + b58 + "TST"
+
+print("YOUR testnet WALLET IS: " + wallet)
+print("YOUR SEED PHRASE IS: " + seed)
   

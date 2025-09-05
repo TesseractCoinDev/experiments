1d60fcf93b281d80cf0922291366fa0dc7587773
ISTEK <tamdacoder@gmail.com>
2025-09-05 12:54:21 -0500

    Update linear.py

--- mining/linear.py
+++ mining/linear.py
@@ -6,16 +6,15 @@ import os
 coefficient = 65535
 exponent = 29
 target = coefficient * 2**(8*(exponent - 3))
-kek = keccak.new(digest_bits=256)
 
-def hex():
+while True:
   version = "1.0.0"
   prevHashes = [keccak(os.urandom(2)).hex() for _ in range(6)]
   merkle = keccak(os.urandom(2)).hex()
   timestamp = str(time.time())
   tar = target
   nonce = random.getrandbits(32)
-  s = version.encode("utf-8") + prevHashes.encode("utf-8") + merkle.encode("utf-8") + timestamp.encode("utf-8")
+  s = (version.encode() + "".join(prevHashes).encode() + merkle.encode() + timestamp.encode())
   sf = s + tar.to_bytes(32, "big") + nonce.to_bytes(4, "big")
   hexHash = keccak(sf).hex()
   hexHeader = [{"verison": version, "prevHashes": prevHashes, "merkleRoot": merkle, "timestamp": timestamp, "target": str(tar), "nonce": str(nonce), "hexHash": hexHash}]
@@ -24,10 +23,5 @@ def hex():
   if int(hexHash, 16) <= target:
     print("Mined")
     break
-  else:
-    hex()
-
-while True:
-  hex()
   
   

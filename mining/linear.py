177efdd9f87f1d2f8e1c73e355707d8cece04252
ISTEK <tamdacoder@gmail.com>
2025-09-05 12:50:14 -0500

    Update linear.py

--- mining/linear.py
+++ mining/linear.py
@@ -1,26 +1,29 @@
-from sha3 import keccak_256
+from eth_utils import keccak
 import random
+import time
 import os
 
 coefficient = 65535
 exponent = 29
-target = coefficient * 2**8*(exponent - 3)
+target = coefficient * 2**(8*(exponent - 3))
+kek = keccak.new(digest_bits=256)
 
 def hex():
   version = "1.0.0"
-  prevHashes = [sha3.keccak_256(os.urandom(2)).hexdigest(), sha3.keccak_256(os.urandom(2)).hexdigest(), sha3.keccak_256(os.urandom(2)).hexdigest(), sha3.keccak_256(os.urandom(2)).hexdigest(), sha3.keccak_256(os.urandom(2)).hexdigest(), sha3.keccak_256(os.urandom(2)).hexdigest()]
-  merkle = sha3.keccak_256(os.urandom(2)).hexdigest()
+  prevHashes = [keccak(os.urandom(2)).hex() for _ in range(6)]
+  merkle = keccak(os.urandom(2)).hex()
   timestamp = str(time.time())
   tar = target
   nonce = random.getrandbits(32)
   s = version.encode("utf-8") + prevHashes.encode("utf-8") + merkle.encode("utf-8") + timestamp.encode("utf-8")
   sf = s + tar.to_bytes(32, "big") + nonce.to_bytes(4, "big")
-  hexHash = sha3.keccak_256(sf).hexdigest()
+  hexHash = keccak(sf).hex()
   hexHeader = [{"verison": version, "prevHashes": prevHashes, "merkleRoot": merkle, "timestamp": timestamp, "target": str(tar), "nonce": str(nonce), "hexHash": hexHash}]
   print("Mining - Current Hash:" + hexHash)
   
   if int(hexHash, 16) <= target:
     print("Mined")
+    break
   else:
     hex()
 

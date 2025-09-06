4873466ea42e31599d7c01ea9450d7a156b8dbeb
ISTEK <tamdacoder@gmail.com>
2025-09-06 14:20:05 -0500

    Update hexheader.py

--- hex/hexheader.py
+++ hex/hexheader.py
@@ -11,9 +11,9 @@ def header():
   merkle = keccak(os.urandom(2))
   timestamp = str(time.time())
   op = 2**256 // 2**20
-  subtargets = [random.getrandbits(op) for _ in range(768)]
+  part = [keccak(os.urandom(2) for _ in range(768)]
   finalized = False
-  static = version.encode("utf-8") + prevHashes + merkle + timestamp.encode("utf-8") + subtargets.to_bytes(30, "big") + str(finalized).encode("utf-8")
+  static = version.encode("utf-8") + prevHashes + merkle + timestamp.encode("utf-8") + part.encode("utf-8") + str(finalized).encode("utf-8")
   hexHash = keccak(keccak(static)).hex()
   return {
     "version": version,
@@ -21,8 +21,26 @@ def header():
     "merkleRoot": merkle.hex(),
     "finalized": finalized,
     "timestamp": timestamp,
-    "subtargets": subtargets,
+    "partitions": part.hex(),
     "hexHash": hexHash
   }
-  
+
+def partition():
+  version = "1.0.0"
+  timestamp = str(time.time())
+  transactions = [keccak(os.urandom(2)) for _ in range(8)]
+  merkle = keccak(keccak(keccak(transactions[1] + transactions[2]) + keccak(transactions[3] + transactions[4])) + keccak(keccak(transactions[5] + transactions[6]) + keccak(transactions[7] + transactions[8])))
+  t = 2**256 // 2**20
+  nonce = random.getrandbits(32)
+  static = version.encode("utf-8") + timestamp.encode("utf-8")
+  partitionHash = keccak(keccak((static + transactions + merkle + t.to_bytes(32, "big") + nonce.to_bytes(16, "big")))).hex()
+  return {
+    "version": version,
+    "timestamp": timestamp,
+    "merkleRoot": merkle.hex(),
+    "transactions": transactions.hex(),
+    "target": t.hex(),
+    "nonce": nonce.hex(),
+    "partitionHash": partitionHash
+  }
   

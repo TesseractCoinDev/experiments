268150f6d99b6f5356a1d415476de16f52cfe0b5
ISTEK <tamdacoder@gmail.com>
2025-09-06 11:25:58 -0500

    Update gen.py\

--- wallet/gen.py
+++ wallet/gen.py
@@ -32,42 +32,8 @@ def begin():
 
 begin()
 
-def hex():
-  timestamp = str(time.time())
-  merkle = [keccak(os.urandom(2)).hex() for _ in range(6)]
-  to = "X" + base58.b58encode(keccak(os.urandom(32))).decode() + "TSP"
-  froms = "X" + base58.b58encode(keccak(os.urandom(32))).decode() + "TSP"
-  nonce = random.getrandbits(128)
-  difficulty = 2**20
-  target = 2**256 // difficulty
-  prevHash = keccak(os.urandom(32))
-  s = (timestamp.encode("utf-8") + to.encode("utf-8") + froms.encode("utf-8") + prevHash)
-  final = s + target.to_bytes(32, "big") + nonce.to_bytes(16, "big")
-  hexHash = keccak(final).hex()
-  return {
-    "timestamp": timestamp,
-    "merkleRoot": merkle,
-    "to": to,
-    "from": froms,
-    "prevHash": prevHash.hex(),
-    "nonce": nonce,
-    "target": target,
-    "hexHash": hexHash
-  }
-
-def privatenet():
-  encryptionk = pNET(keccak(bytes.fromhex(public)))
-  transaction = json.dumps(hex()).encode()
-  nunce = os.urandom(12)
-
-  ctxt = encryptionk.encrypt(nunce, transaction, None)
-  return ctxt
-
-begin()
-
 def display():
-  print(colored(f"YOUR pnet WALLET IS: {wallet}", "green", attrs=["bold"]))
+  print(colored(f"YOUR TESTNET WALLET IS: {wallet}", "green", attrs=["bold"]))
   print(colored(f"YOUR PUBLIC KEY IS: {public}", "green", attrs=["bold"]))
   print(colored(f"YOUR PRIVATE KEY IS: {private}", "green", attrs=["bold"]))
   print(colored(f"YOUR SEED PHRASE IS: {mn}", "green", attrs=["bold"]))
-  print(colored(f"YOUR ENCRYPTED TRANSACTION IS: {privatenet().hex()}", "green", attrs=["bold"]))

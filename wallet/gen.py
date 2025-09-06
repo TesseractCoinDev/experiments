37490c901e2c2f8384c494f5203cd9e520455ac4
ISTEK <tamdacoder@gmail.com>
2025-09-05 23:07:32 -0500

    Update gen.py

--- wallet/gen.py
+++ wallet/gen.py
@@ -1,8 +1,12 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM as pNET
 from bip32utils import BIP32Key
 from mnemonic import Mnemonic
 from termcolor import colored
 from eth_utils import keccak
+import random
 import base58
+import time
+import json
 import os
 
 private = ""
@@ -24,13 +28,46 @@ def begin():
 
   kek = keccak(master.PublicKey())
   b58 = base58.b58encode(kek).decode()
-  wallet = "X" + b58 + "TST"
+  wallet = "X" + b58 + "TSP"
+
+begin()
+
+def hex():
+  timestamp = str(time.time())
+  merkle = [keccak(os.urandom(2)).hex() for _ in range(6)]
+  to = "X" + base58.b58encode(keccak(os.urandom(32))).decode() + "TSP"
+  froms = "X" + base58.b58encode(keccak(os.urandom(32))).decode() + "TSP"
+  nonce = random.getrandbits(128)
+  difficulty = 2**20
+  target = 2**256 // difficulty
+  prevHash = keccak(os.urandom(32))
+  s = (timestamp.encode("utf-8") + to.encode("utf-8") + froms.encode("utf-8") + prevHash)
+  final = s + target.to_bytes(32, "big") + nonce.to_bytes(16, "big")
+  hexHash = keccak(final).hex()
+  return {
+    "timestamp": timestamp,
+    "merkleRoot": merkle,
+    "to": to,
+    "from": froms,
+    "prevHash": prevHash.hex(),
+    "nonce": nonce,
+    "target": target,
+    "hexHash": hexHash
+  }
+
+def privatenet():
+  encryptionk = pNET(keccak(bytes.fromhex(public)))
+  transaction = json.dumps(hex()).encode()
+  nunce = os.urandom(12)
+
+  ctxt = encryptionk.encrypt(nunce, transaction, None)
+  return ctxt
 
 begin()
 
 def display():
-  print(colored(f"YOUR testnet WALLET IS: {wallet}", "green", attrs=["bold"]))
+  print(colored(f"YOUR pnet WALLET IS: {wallet}", "green", attrs=["bold"]))
   print(colored(f"YOUR PUBLIC KEY IS: {public}", "green", attrs=["bold"]))
   print(colored(f"YOUR PRIVATE KEY IS: {private}", "green", attrs=["bold"]))
   print(colored(f"YOUR SEED IS: {seed.hex()}", "green", attrs=["bold"]))
-  
+  print(colored(f"YOUR ENCRYPTED TRANSACTION IS: {privatenet().hex()}", "green", attrs=["bold"]))

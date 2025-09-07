d4ac50d56cf6e3c59aedf2cf4eb7aa008dabfc33
ISTEK <tamdacoder@gmail.com>
2025-09-06 23:07:20 -0500

    Update hexcontent.py

--- hex/hexcontent.py
+++ hex/hexcontent.py
@@ -1,3 +1,5 @@
+from bip32utils import BIP32Key
+from mnemonic import Mnemonic
 from termcolor import colored
 from eth_utils import keccak
 import random
@@ -6,18 +8,28 @@ import ecdsa
 import time
 import os
 
+public = ""
+private = ""
+
+def begin():
+  global private, public
+  entropy = os.urandom(24)
+  mn = init.to_mnemonic(entropy)
+  seed = init.to_seed(mn, passphrase="")
+  master = BIP32Key.fromEntropy(seed)
+  private = master.PrivateKey().hex()
+  public = master.PublicKey().hex()
+
 def transaction():
     to = "X" + base58.b58encode(keccak(os.urandom(2))).decode() + "TST"
     From = "X" + base58.b58encode(keccak(os.urandom(2))).decode() + "TST"
     amount = random.randint(1, 100)
-    timestamp = str(time.time())
-    s = ecdsa.SigningKey.from_string(os.urandom(32), curve=ecdsa.SECP256K1)
-    v = s.verifying_key
-    b = v.to_string()
-    signature = b.hex()
-    lens = len(to) + len(From) + len(amount) + len(timestamp) + len(signature)
+    timestamp = time.time()
+    sk = SigningKey.from_string(bytes.fromhex(private), curve=SECP256k1)
+    signature = keccak(to + From + str(amount) + str(timestamp)).sign().hex()
+    lens = len(to) + len(From) + len(str(amount)) + len(str(timestamp)) + len(signature)
     fee = 0.0001 * lens
-    txid = keccak(to.encode("utf-8") + From.encode("utf-8") + str(amount).encode("utf-8") + timestamp.encode("utf-8") + signature.encode("utf-8") + fee.encode("utf-8"))
+    txid = keccak(to.encode("utf-8") + From.encode("utf-8") + str(amount).encode("utf-8") + timestamp.encode("utf-8") + signature.encode("utf-8") + fee.encode("utf-8") + public.encode("utf-8"))
     return txid
 
 def partition():

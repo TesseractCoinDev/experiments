20a10b1df77d577bbc9a3ed1e1a4a97b52f88fe1
ISTEK <tamdacoder@gmail.com>
2025-09-14 21:38:13 -0500

    Update gen.py

--- wallet/gen.py
+++ wallet/gen.py
@@ -1,4 +1,5 @@
 from bip32utils import BIP32Key
+from Crypto.Hash import RIPEMD
 from mnemonic import Mnemonic
 from termcolor import colored
 from eth_utils import keccak
@@ -25,7 +26,8 @@ def begin():
   testnet = bytes([0x54])
   mainnet = bytes([0x58])
   pnet = bytes([0x50])
-  pubkeyh = keccak(master.PublicKey())[:20]
+  ripemd = RIPEMD.new(master.PublicKey())
+  pubkeyh = ripemd.digest()
   checksum = keccak(keccak(testnet + pubkeyh))[:4]
   b58 = base58.b58encode(testnet + pubkeyh + checksum).decode()
 

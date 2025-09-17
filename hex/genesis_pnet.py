fee285debd297fb5d6f22522250e9d4a6c5ead3a
ISTEK <tamdacoder@gmail.com>
2025-09-17 12:37:25 -0500

    Update genesis_pnet.py

--- hex/genesis_pnet.py
+++ hex/genesis_pnet.py
@@ -45,7 +45,7 @@ Wallet = wallet
 gen()
 Wallet2 = wallet
 publicKey2 = public
-nonce = int(keccak(keccak(publicKey2 + str(time.time()).encode("utf-8"))), 12)
+nonce = int(keccak(keccak(publicKey2)).hex(), 16)
 pnet = AESGCM(publicKey2)
 
 def ptransaction():
